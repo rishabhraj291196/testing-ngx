8b498a151ac471450581d9d5c8fa047fa0ff43e5
Rishabh Azad <rishabhraj291196@gmail.com>
2020-01-24 22:50:42 +0530

    http loader working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,76 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatInputModule,
+         MatSelectModule, MatSlideToggleModule, MatSliderModule,
+         MatCheckboxModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ColorPickerModule } from 'ngx-color-picker';
+
+import { NgxUiLoaderModule, NgxUiLoaderConfig, SPINNER, POSITION,
+  PB_DIRECTION, NgxUiLoaderRouterModule, NgxUiLoaderHttpModule } from 'ngx-ui-loader';
+
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+import { NgxUiLoaderDemoComponent } from './ngxUiLoaderDemo/ngx-ui-loader-demo.component';
+
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  bgsColor: 'red',
+  // bgsOpacity: 0.5,
+  // bgsPosition: POSITION.bottomCenter,
+  // bgsSize: 60,
+  // bgsType: SPINNER.rectangleBounce,
+  fgsColor: 'red',
+  // fgsPosition: POSITION.centerCenter,
+  // fgsSize: 60,
+  // fgsType: SPINNER.chasingDots,
+  // logoUrl: 'assets/angular.png',
+  pbColor: 'red',
+  // pbDirection: PB_DIRECTION.leftToRight,
+  // pbThickness: 5,
+  // text: 'Welcome to ngx-ui-loader',
+  // textColor: '#FFFFFF',
+  // textPosition: POSITION.centerCenter
+};
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NgxUiLoaderDemoComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgxUiLoaderDemoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ColorPickerModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatListModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderRouterModule, // import this module for showing loader automatically when navigating between app routes
+    NgxUiLoaderHttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
